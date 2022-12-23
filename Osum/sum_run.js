bf0de9a2b9dce35fe61807b50365f1e35bd1cf2c
Yura <yukokanin@chr.rsvo.ru>
2022-12-23 14:07:20 +0300

    fixed whitespaces in sum_run.js

--- Osum/sum_run.js
+++ Osum/sum_run.js
@@ -12,27 +12,27 @@ const test_data = prepareTestData()
 
 //логирование (два стрима: консоль и файл)
 function createLogger(fileName) {
-    var prettyStdOut = new PrettyStream();
-    var prettyFileOut = new PrettyStream();
-    var myFile = createWriteStream(fileName);
-    prettyStdOut.pipe(process.stdout);
-    prettyFileOut.pipe(myFile);
-    let log = bunyan.createLogger({
-        name: 'test',
-        streams: [
-                    {
-                        level:bunyan.INFO,
-                        stream: prettyStdOut
-                    },
-                    {
-                        path: fileName,
-                        level:bunyan.INFO,
-                        useColor: false,
-                        stream: prettyFileOut
-                    }
-                ]
-    });
-    return log
+	var prettyStdOut = new PrettyStream();
+	var prettyFileOut = new PrettyStream();
+	var myFile = createWriteStream(fileName);
+	prettyStdOut.pipe(process.stdout);
+	prettyFileOut.pipe(myFile);
+	let log = bunyan.createLogger({
+		name: 'test',
+		streams: [
+			{
+				level:bunyan.INFO,
+				stream: prettyStdOut
+			},
+			{
+				path: fileName,
+				level:bunyan.INFO,
+				useColor: false,
+				stream: prettyFileOut
+			}
+		]
+	});
+	return log
 }
 
 // уровни логирования: "fatal" (60); "error" (50); "warn" (40); "info"(30); "debug" (20); "trace" (10)
